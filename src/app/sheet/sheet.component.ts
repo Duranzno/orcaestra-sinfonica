a7c3b0eeac9c27ae2789e401846ae9eea742de8c
Duranzno <aledurax@gmail.com>
2018-12-20 16:53:14 -0400

    OSMD works but its too heavy

--- src/app/sheet/sheet.component.ts
+++ src/app/sheet/sheet.component.ts
@@ -17,22 +17,32 @@ export class SheetComponent implements OnInit {
   ngOnInit() {
   	var container = document.createElement("div");
   	document.body.appendChild(container);
-  	this.osmd = new OpenSheetMusicDisplay(container, { autoResize: false });
-  	// this.loadMusicXML("assets/music.xml");
-  }
+	this.osmd = new OpenSheetMusicDisplay(container, { autoResize: false });
+	this.loadMusicXML("https://raw.githubusercontent.com/opensheetmusicdisplay/opensheetmusicdisplay/develop/test/data/ActorPreludeSample.xml");
+	}
  	loadMusicXML(url) {
- 		let ethis=this;
+ 		let ethis=this;  
 		let xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function () {
 			switch (xhttp.readyState) {
 				case 0: // UNINITIALIZED
+					console.log("uninitialized");
+					break;
 				case 1: // LOADING
+					console.log("loading");
+					break;
 				case 2: // LOADED
+					console.log("loaded");
+					break;
 				case 3: // INTERACTIVE
-				break;
+					console.log("interactive");
+					break;
 				case 4: // COMPLETED
-					ethis.osmd.load(xhttp.responseXML);
-					ethis.osmd.render();
+					// ethis.osmd.load(xhttp.response);
+					// ethis.osmd.render();
+					// console.log("completed");
+					// console.log(xhttp.responseXML);
+					// console.log(xhttp);
 					break;
 				default:
 				throw ("Error loading MusicXML.");
