10021b75eccfbf1ee66a0218bb316f65083353a3
alejandro <aledurax@gmail.com>
2019-03-22 12:36:18 -0400

    SHould Fix Travisci

--- src/app/music/score/media-cards/media.card.component.ts
+++ src/app/music/score/media-cards/media.card.component.ts
@@ -5,7 +5,7 @@ import { YoutubeService } from '../../services';
 @Component({
   selector: 'app-media-card',
   template: `
-  <span [ngSwitch]="media.type">
+  <span [ngSwitch]="media.tipo">
     <div *ngSwitchCase="types.MP3"><app-mp3 [media]="media"></app-mp3></div>
     <div *ngSwitchCase="types.YOUTUBE"><app-youtube [media]="media"></app-youtube></div>
 </span>`,
